dbc5d4f94910da8d435911768c770fb1f48f97e2
Sreekanth Ps <sreekanth@shazab.in>
2024-10-16 12:20:03 +0530

    update env files

---
+++ myWorkProblems/updateEnv.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+function updateEnvVariable(updateData) {
+  try {
+    const envFilePath = "./.env";
+    const envVars = fs.readFileSync(envFilePath, "utf8").split("\n");
+
+    const envVarsObj = {};
+    const updatedVars = [];
+
+    envVars.forEach((useLine) => {
+      const [key, value] = useLine.split("=");
+      if (key) {
+        envVarsObj[key] = value;
+      }
+    });
+
+    updateData.forEach(({ key, value }) => {
+      envVarsObj[key] = value;
+    });
+
+    for (const key in envVarsObj) {
+      if (envVarsObj.hasOwnProperty(key)) {
+        updatedVars.push(`${key}=${envVarsObj[key]}`);
+      }
+    }
+
+    fs.writeFileSync(envFilePath, updatedVars.join("\n"));
+    console.log("done", updatedVars.join("\n"));
+  } catch (error) {
+    console.log(`Failed to update .env file: ${error.stack || error.message}`);
+    throw error;
+  }
+}
+
+updateEnvVariable([
+  { key: "ACCESS_TOKEN", value: "AccessToken4444" },
+  { key: "REFRESH_TOKEN", value: "RefreshToken4444" },
+]);
